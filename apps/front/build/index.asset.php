53ba53703d584a2cefc95209714c5c5758253760
Casey Milne <cjmofficio@gmail.com>
2025-10-14 15:28:41 -0700

    Update front build.

--- apps/front/build/index.asset.php
+++ apps/front/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-element'), 'version' => '937c620c3152328714b5');
+<?php return array('dependencies' => array('react', 'wp-element'), 'version' => '7057e2826d851ca0449f');
